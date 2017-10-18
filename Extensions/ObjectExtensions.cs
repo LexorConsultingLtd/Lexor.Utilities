2dcfc4e2629a049e374f63a433604e3624060f91
Steve Lamotte <steve@lexor.ca>
2017-10-18 16:19:57 -0500

    Reorganized project structure

---
+++ Extensions/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexor.Extensions
+{
+    public static class ObjectExtensions
+    {
+        // Need to invoke with "this" e.g. this.GetPrivateProperty(...)
+
+        public static object GetPrivateProperty(this object instance, Type type, string propertyName)
+        {
+            var bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+            FieldInfo field = type.GetField(propertyName, bindFlags);
+            return field?.GetValue(instance);
+        }
+
+        public static void SetPrivateProperty(this object instance, Type type, string propertyName, object value)
+        {
+            var bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+            FieldInfo field = type.GetField(propertyName, bindFlags);
+            field?.SetValue(instance, value);
+        }
+    }
+}
---
+++ Resources.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexor.Utilities
+{
+    public class Resources
+    {
+        public static async Task<string> ReadResourceAsync(string resourceName, Assembly resourceAssembly = null)
+        {
+            if (resourceAssembly == null) resourceAssembly = Assembly.GetEntryAssembly();
+            var stream = resourceAssembly.GetManifestResourceStream(resourceName);
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
