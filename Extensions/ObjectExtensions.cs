715fc635cb22dab895afca2c1aa20e118ece4d9a
Steve Lamotte <steve@lexor.ca>
2017-11-14 11:23:53 -0700

    Reorganized project structure to match reference projects

--- Extensions/ObjectExtensions.cs
+++ Extensions/ObjectExtensions.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lexor.Extensions
+namespace Lexor.Utilities.Extensions
 {
     public static class ObjectExtensions
     {
