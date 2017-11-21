a40ecd63bafc8856a639a2c30071ae4f2903b1b3
Steve Lamotte <steve@lexor.ca>
2017-11-21 01:34:34 -0700

    VotingLocation model and seed data working

---
+++ Extensions/DbContextExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Lexor.Utilities.Extensions
+{
+    public static class DbContextExtensions
+    {
+        public static void ApplyModelConfigurations(this DbContext context, ModelBuilder modelBuilder)
+        {
+            var configTypes = Assembly.GetAssembly(context.GetType())
+                .GetTypes()
+                .Where(t => !t.IsAbstract)
+                .SelectMany(t => t.GetInterfaces().Select(i => new { ConfigClass = t, Interface = i }))
+                .Where(c => c.Interface.IsGenericType && c.Interface.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>))
+                .Select(c => new { c.ConfigClass, GenericType = c.Interface.GenericTypeArguments.FirstOrDefault() })
+                .ToList();
+            foreach (var configType in configTypes)
+            {
+                var config = Activator.CreateInstance(configType.ConfigClass);
+                var method = typeof(ModelBuilder)
+                    .GetMethod(nameof(modelBuilder.ApplyConfiguration))
+                    .MakeGenericMethod(configType.GenericType);
+                method.Invoke(modelBuilder, new[] { config });
+            }
+        }
+    }
+}
+
+
--- SeedWork/ConfigurationBase.cs
+++ SeedWork/ConfigurationBase.cs
@@ -1,7 +1,27 @@
-﻿namespace Utilities.SeedWork
+﻿using Lexor.Utilities.SeedWork;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Utilities.SeedWork
 {
     public abstract class ConfigurationBase
     {
-        protected static string FkColName(string fieldName) => $"{fieldName}Id";
+        public virtual void ConfigureEntity<T>(EntityTypeBuilder<T> config) where T : Entity
+        {
+            config.Property(t => t.Id).IsRequired();
+            config.HasKey(t => t.Id);
+            config.Ignore(t => t.DomainEvents);
+        }
+
+        protected static string FkColName(Type entityType) => FkColName(entityType.Name);
+        protected static string FkColName(string entityName) => $"{entityName}Id";
+
+        protected static void DefineForeignKey<T>(EntityTypeBuilder<T> config, Type entityType) where T : Entity
+        {
+            var columnName = FkColName(entityType);
+
+            config.Property<int>(columnName).IsRequired();
+            config.HasIndex(columnName);
+        }
     }
 }
