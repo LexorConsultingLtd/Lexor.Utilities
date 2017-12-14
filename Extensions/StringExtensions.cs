d8fecdf3c58074691999df5ce6cfc1a8a040cbbe
Steve Lamotte <steve@lexor.ca>
2017-12-14 13:08:36 -0600

    DRY up field display

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lexor.Utilities.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string SplitCamelCase(this string str)
+        {
+            return Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
+        }
+    }
+}
