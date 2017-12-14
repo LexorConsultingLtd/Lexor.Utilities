08deafef04c28fe2ee91fd2c04cf6a596056c35b
Steve Lamotte <steve@lexor.ca>
2017-12-14 16:57:55 -0600

    Change voting locations to do soft deletes

---
+++ EFCore/SoftDeletes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Linq;
+
+namespace Utilities.EFCore
+{
+    public class SoftDeletes
+    {
+        public const string SoftDeleteColumnName = "IsDeleted";
+
+        public static void Enable<T>(EntityTypeBuilder<T> config) where T : class
+        {
+            config.Property<bool>(SoftDeleteColumnName);
+            config.HasQueryFilter(t => !EF.Property<bool>(t, SoftDeleteColumnName));
+        }
+
+        public static void ProcessSoftDeletes(ChangeTracker changeTracker)
+        {
+            var softDeleteEntries = changeTracker.Entries()
+                .Where(e => e.Properties.Any(p => p.Metadata.Name.Equals(SoftDeleteColumnName)));
+            foreach (var entry in softDeleteEntries)
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues[SoftDeleteColumnName] = false;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.CurrentValues[SoftDeleteColumnName] = true;
+                        entry.State = EntityState.Modified;
+                        break;
+                }
+            }
+        }
+    }
+}
