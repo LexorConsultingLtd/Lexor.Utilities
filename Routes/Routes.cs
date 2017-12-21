991b0b7da8918e3dc4fad2898d036420bbfe4f1a
Steve Lamotte <steve@lexor.ca>
2017-12-20 23:02:32 -0600

    Enhanced route handling with some helpers

---
+++ Routes/Routes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Lexor.Utilities.Routes
+{
+    public static class Routes
+    {
+        public static IDictionary<string, string> GetRouteValues(object route)
+        {
+            switch (route)
+            {
+                case null:
+                    return null;
+                case IDictionary<string, string> values:
+                    return values;
+            }
+            return new Dictionary<string, string> { { "id", route.ToString() } };
+        }
+    }
+}
