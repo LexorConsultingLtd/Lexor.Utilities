55dda1c6100365333a8bc142efb8f14cb85acbad
Steve Lamotte <steve@lexor.ca>
2018-01-24 01:37:35 -0600

    Add Dapper-based spatial boundary queries

---
+++ ConnectionFactory/ConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Utilities.ConnectionFactory
+{
+    public class ConnectionFactory
+    {
+        private IConfiguration Configuration { get; }
+
+        public ConnectionFactory(IConfiguration config)
+        {
+            Configuration = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        protected string ConnectionString => Configuration.GetConnectionString("Default");
+
+        public IDbConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
