f7a07a87e9256f30a3212b9c9400693f9f5cea8e
Steve Lamotte <steve@lexor.ca>
2018-03-13 09:52:05 -0500

    Tweak FK declarations to allow deleted entities' FKs to be set to null (e.g. if a VSD is deleted)

--- Extensions/EntityTypeBuilderExtensions.cs
+++ Extensions/EntityTypeBuilderExtensions.cs
@@ -17,22 +17,29 @@ namespace Utilities.Extensions
             config.Ignore(t => t.DomainEvents);
         }
 
-        public static void DefineForeignKey<T>(this EntityTypeBuilder<T> config, Type entityType, bool restrictDeletes = false, bool required = true) where T : Entity
+        public static void DefineForeignKey<T>(this EntityTypeBuilder<T> config, Type entityType, bool required = true) where T : Entity
         {
             var columnName = $"{entityType.Name}Id";
 
             config.Property(columnName).IsRequired(required);
             config.HasIndex(columnName);
 
-            if (restrictDeletes || typeof(Enumeration).IsAssignableFrom(entityType))
+            var deleteBehavior = DeleteBehavior.Cascade;
+
+            // Optional entries: set to null on delete
+            if (!required)
+                deleteBehavior = DeleteBehavior.SetNull;
+
+            // Force enums to Restrict always
+            if (typeof(Enumeration).IsAssignableFrom(entityType))
+                deleteBehavior = DeleteBehavior.Restrict;
+
+            // Change FK delete behaviour if not Cascade (the default)
+            var foreignKeys = config.Metadata.GetForeignKeys()
+                .Where(fk => fk.PrincipalEntityType.ClrType == entityType);
+            foreach (var foreignKey in foreignKeys)
             {
-                // Change FK delete behaviour to Restrict instead of Cascade (the default)
-                var foreignKeys = config.Metadata.GetForeignKeys()
-                    .Where(fk => fk.PrincipalEntityType.ClrType == entityType);
-                foreach (var foreignKey in foreignKeys)
-                {
-                    foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
-                }
+                foreignKey.DeleteBehavior = deleteBehavior;
             }
         }
     }
