ac2af6da6cb25e40bd5dd7a7052a39327c0067d5
Steve Lamotte <steve@lexor.ca>
2018-03-28 17:37:35 -0500

    Replaced literals with constants, DRYed things up, better parameter handling

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 
@@ -6,10 +7,11 @@ namespace Utilities.Extensions
 {
     public static class StringExtensions
     {
-        public static string SplitCamelCase(this string str)
-        {
-            return Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
-        }
+        public static string SplitCamelCase(this string str) =>
+            Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
+
+        public static string SplitCamelCase(this Enum e) =>
+            e.ToString().SplitCamelCase();
 
         public static string RemoveAccents(this string text) =>
             HttpUtility.UrlDecode(HttpUtility.UrlEncode(text, Encoding.GetEncoding("iso-8859-7")));
