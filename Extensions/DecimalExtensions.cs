565b417687fa1505bd6d2541524b852e1cf7f86f
Steve Lamotte <steve@lexor.ca>
2018-06-26 09:50:14 -0500

    Add extension to format Currency values

---
+++ Extensions/DecimalExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Utilities.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static string FormatCurrency(this decimal value, int numDecimals = 2) =>
+            value.ToString($"C{numDecimals}");
+    }
+}
