5c1ae940e6c3f08c051e0ff9388f41f6c03f1399
Steve Lamotte <steve@lexor.ca>
2018-06-25 21:04:02 -0500

    Create extension method for building select lists from Entity objects

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utilities.SeedWork;
+
+namespace Utilities.Extensions
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<SelectListItem> AsSelectListItems<T>(
+            this IEnumerable<T> entities,
+            Func<T, string> valueExpr
+        ) where T : Entity =>
+            entities.Select(i => new SelectListItem(valueExpr.Invoke(i), i.Id.ToString()));
+    }
+}
