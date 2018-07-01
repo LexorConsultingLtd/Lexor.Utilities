a117126302b449d6b54764714edf39065d2e11d6
Steve Lamotte <steve@lexor.ca>
2018-06-30 23:32:25 -0500

    Add overload to specify text value for null when using nullable bool

--- Extensions/BooleanExtensions.cs
+++ Extensions/BooleanExtensions.cs
@@ -3,5 +3,8 @@
     public static class BooleanExtensions
     {
         public static string FormatYesNo(this bool value) => value ? "Yes" : "No";
+
+        public static string FormatYesNo(this bool? value, string nullText = "") =>
+            value.HasValue ? value.Value.FormatYesNo() : nullText;
     }
 }
