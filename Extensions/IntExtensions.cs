80de0b55176f7109ff150a20fb64f14ed4cde1d7
Steve Lamotte <steve@lexor.ca>
2018-06-30 23:33:13 -0500

    Added an int extension for pluralization

---
+++ Extensions/IntExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Utilities.Extensions
+{
+    public static class IntExtensions
+    {
+        public static string Pluralize(this int value, string singular, string plural = null) =>
+            value == 1 ? $"1 {singular}" : $"{value:D} {(string.IsNullOrEmpty(plural) ? $"{singular}s" : plural)}";
+    }
+}
