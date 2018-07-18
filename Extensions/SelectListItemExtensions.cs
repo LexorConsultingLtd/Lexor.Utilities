e4d574afaabf5f2d31fe5335701ccca879aee3bf
Steve Lamotte <steve@lexor.ca>
2018-07-17 23:42:32 -0500

    Add worker-based assignments. Several enhancements to worker assignments across all areas. See #125

---
+++ Extensions/SelectListItemExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Utilities.Extensions
+{
+    public static class SelectListItemExtensions
+    {
+        public static List<SelectListItem> NewListWithSelectItem(
+            IEnumerable<SelectListItem> items,
+            string selectItemText = "-- Select --"
+        )
+        {
+            var result = new List<SelectListItem> { new SelectListItem(selectItemText, "") };
+            result.AddRange(items);
+            return result;
+        }
+    }
+}
